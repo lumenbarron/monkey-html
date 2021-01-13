8b18c34a5d21d7f42113fd7e7b572f34288b72df
lumenbarron <lucemendez@gmail.com>
2021-01-13 16:59:29 -0600

    translate to spanish

---
+++ components-spanish/brandingCostumers-es.js
@@ -0,0 +1,40 @@
+class BrandingCostumer extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="container-fluid container-brand border-test">
+      <div class="row container-text">
+      <p class="supplier-text-icons pb-1">
+      Atendemos a las mayores empresas, si usted es proveedor de algunas de ellas, usted puede adelantar sus pagos en nuestro programa.
+      </p>
+      </div>
+      <div class="row container-logo-one">
+        <img src='./images/natura.png' class="brand-size" alt="natura-company" />
+        <img src='./images/philco.png' id="philco" class="brand-size" alt="philco-company" />
+        <img src='./images/ifood.png' id="ifood" class="brand-size" alt="ifood-company" />
+        <img src='./images/braskem.png' id="braskem" class="brand-size" alt="braskem-company" />
+      </div>
+      <div class="row container-logo-two">
+        <img src='./images/petrobras.png' id="petrobras" class="brand-size" alt="petrobras-company" />
+        <img src='./images/minerva.png' class="brand-size" alt="minerva-company" />
+        <img src='./images/mdias.png' id="mdias" class="brand-size" alt="mdias-company" />
+        <img src='./images/kws.png' id="kws" class="brand-size" alt="kws-company" />
+        <img src='./images/gerdau.png' id="gerdau" class="brand-size" alt="gerdau-company" />
+      </div>
+      <div class="row container-text-info">
+      <p class="supplier-text-icons pb-1">
+     ¿ No encontro la empresa que usted es proveedor ? Entre en contacto con nosotros y le
+      responderemos si la empresa poseé nuestra solución.
+      </p>
+      </div>
+      <div class="row container-text">
+      <button class="btn-branding "><a href="#contact"> Entrar en contacto </a></button>
+      </div>
+    </div>
+          `;
+  }
+}
+
+window.customElements.define("branding-costumer", BrandingCostumer);
---
+++ components-spanish/supplier-es.js
@@ -0,0 +1,75 @@
+class Supplier extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+          <div class="container-fluid" id="supplier">
+            <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-6 order-two border-test flex-general p-0">
+                <img
+                src='./images/supplier-banner-es.png'
+                class="supplier-banner"
+                alt="supplier-banner"
+              />
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-6 order-one border-test supplier-container">
+              <button class="btn-supplier mb-4">Proveedor</button>  
+              <h2 class="title-h2">
+                  ¿ Notas Fiscales para recibir sólo de aqui a 30, 45 o 120 días? Adelantalas ahora mismo.
+                </h2>
+                <h2 class="title-h2">
+                Adelantar ahora mismo.
+              </h2>
+                <p class="main-text">
+                Garantizamos la mejor taza para usted, así podrá descontar de su Nota Fiscal: productos, servicios o transporte. Así, usted recibe más dinero, en el mismo día.
+                </p>
+                <p class="main-text">
+                Con ese dinero usted puede pagar una deuda más cara o invertir en su negocio para vender más.
+                </p>
+                <a class="link-style" href="#contact">
+                  Saber más sobre las ventajas.
+                </a>
+              </div>
+            </div>
+            <div class="row icons-container border-test">
+              <div class="col text-icons">
+                <img src='./images/calendar.svg' class="pb-3" alt="calendar-icon" />
+                <div>
+                <h5 class="title-icons">
+                Intereses Competitivos
+                </h5>
+                <p class="supplier-text-icons pb-1">
+                Nuestros intereses es en promedio 70% menor que se cobra en el mercado.
+                </p>
+                </div>
+              </div>
+              <div class="col text-icons">
+                <img src='./images/chart.svg' class="pb-3" alt="chart.png-icon" />
+                <div>
+                <h5 class="title-icons">
+                Adelante Notas Fiscales
+                </h5>
+                <p class="supplier-text-icons pb-1">
+                La solución para su Nota Fiscal a partir de 15 días.
+                </p>
+                </div>
+              </div>
+              <div class="col text-icons">
+                <img src='./images/wallet.svg' class="pb-3" alt="wallet.svg-icon" />
+                <div>
+                <h5 class="title-icons">
+                Dinero en el mismo día
+                </h5>
+                <p class="supplier-text-icons pb-1">
+                Nuestros inversionistas depositan el dinero en el mismo día.
+                </p>
+                </div>
+                </div>
+            </div>
+          </div>
+          `;
+    }
+  }
+  
+  window.customElements.define("supplier-section", Supplier);
