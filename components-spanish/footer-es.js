e378258ce5d01a7191e963caefbc47a9ef210c88
pako10 <pakoandrade10@gmail.com>
2021-02-16 11:48:26 -0600

    Text revision
    Contains the revision of some texts

--- components-spanish/footer-es.js
+++ components-spanish/footer-es.js
@@ -19,7 +19,7 @@ class Footer extends HTMLElement {
           <h6>Institucional</h6>
           <p>Sobre Nosotros</p>
           <a class="text-footer" target="_blank" href="http://help.monkey.exchange/pt-BR/"><p>Dudas</p></a>
-          <a class="text-footer" target="_blank" href="quem-somos-es.html"><p>Quienes Somos</p></a>
+          <a class="text-footer" target="_blank" href="quem-somos-es.html"><p>Quiénes Somos</p></a>
         </div>
         <div class="col-sm-12 col-md-3 col-lg-3 titles-footer">
           <h6>Redes Sociales</h6>
@@ -59,8 +59,8 @@ class Footer extends HTMLElement {
       </div>
       <div class="row m-0 row-copyright">
         <p class="copyright-footer">
-          © Monkey Exchange 2021. Todos los derechos reservados. Acesse nuestra
-          <a> Política de Privacidad.</a>
+          © Monkey Exchange 2021. Todos los derechos reservados. Acceda a nuestras
+          <a> Políticas de Privacidad.</a>
         </p>
       </div>
       <form onchange="selectLang()">
